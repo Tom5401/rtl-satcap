c6e1c9a7030cd659dc3148c5c481bd83c71aa5e1
Tom5401 <tom.nielen@hotmail.nl>
2024-03-25 20:36:11 +0100

    Added install.sh with rtl sdr driver installation

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Install rtl sdr v4 drivers
+# Remove old drivers
+sudo apt purge ^librtlsdr
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_* 
+# Install drivers
+sudo apt install libusb-1.0-0-dev git cmake pkg-config
+git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+cd rtl-sdr-blog
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+cd ~/
+
+
