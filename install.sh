625e5931c6a913b23a526d57e2db0b9bfd1112ea
Tom5401 <tom.nielen@hotmail.nl>
2024-04-03 21:41:49 +0200

    Correct path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo apt purge ^librtlsdr
 sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_* 
 # Install drivers
-sudo apt install libusb-1.0-0-dev git cmake pkg-config
+sudo apt -y install libusb-1.0-0-dev git cmake pkg-config
 git clone https://github.com/rtlsdrblog/rtl-sdr-blog
 cd rtl-sdr-blog
 mkdir build
@@ -16,11 +16,11 @@ sudo make install
 sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
 sudo ldconfig
 echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
-cd ..
+cd ../..
 
 # Install packages
 cd packages
-sudo dpkg -i packages/noaa-apt_1.4.1-1_amd64.deb
+sudo dpkg -i ./noaa-apt_1.4.1-1_amd64.deb
 sudo dpkg -i ./wxtoimg-amd64-2.11.2-beta.deb
 
 sudo dpkg -i ./satdump_1.1.4_amd64.deb
