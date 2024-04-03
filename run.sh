f0389c23cf672cced8d97d82958d698cd398608f
Tom5401 <tom.nielen@hotmail.nl>
2024-04-03 22:51:48 +0200

    home dir

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HOME_DIR=$(pwd)
+cd rtl-satcap
+
 # Install packages
 sudo sh ./install_packages.sh
 
@@ -8,6 +11,6 @@ sudo sh ./install_packages.sh
 
 # setup predict
 wget "https://celestrak.org/NORAD/elements/gp.php?GROUP=weather&FORMAT=tle" -O config/predict/predict.tle
-cp config/predict/predict.tle ~/.predict/predict.tle
-cp config/predict/predict.qth ~/.predict/predict.qth
+cp config/predict/predict.tle $HOME_DIR/.predict/predict.tle
+cp config/predict/predict.qth $HOME_DIR/.predict/predict.qth
 
